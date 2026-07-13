3b847f96bd0a903c88bd006e60586de4f4c93912
Ric Charles Paquibot <ric2thiss@gmail.com>
2026-07-13 20:47:24 +0800

    Fix visitor IP tracking for proxies and Cloudflare

--- index.php
+++ index.php
@@ -3,6 +3,13 @@
 // Track Visitor
 try {
     $ip = $_SERVER['REMOTE_ADDR'];
+    if (!empty($_SERVER['HTTP_CF_CONNECTING_IP'])) {
+        $ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ipList = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+        $ip = trim($ipList[0]);
+    }
+    
     $date = date('Y-m-d');
     
     // Check if IP already visited today
